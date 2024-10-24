7eb101fb9eec784e7c4034ce2336770ab18ff17b
Balan Mihai <mihai.balan.bala@gmail.com>
2024-10-24 18:32:02 +0300

    adaugiri diverse.. faine!

--- my-app/src/components/TripDetail.tsx
+++ my-app/src/components/TripDetail.tsx
@@ -181,9 +181,63 @@ const TripDetail: React.FC<{ user: any }> = ({ user }) => {
 
   return (
     <div className="max-w-3xl mx-auto p-8 my-4 bg-white shadow-xl rounded-2xl lg:max-w-4xl">
-      <h2 className="text-3xl lg:text-5xl font-extrabold text-gray-900 mb-8">
-        {trip.tripName}
-      </h2>
+      <div className="flex justify-between">
+        <h2 className="text-3xl lg:text-5xl font-extrabold text-gray-900 mb-8">
+          {trip.tripName}
+        </h2>
+
+        {/* Author-only Actions */}
+        {user?.displayName === trip.authorName && (
+          <div className="mt-2">
+            <div className="flex gap-2">
+              {!trip.chatId && (
+                <button
+                  onClick={handleCreateChat}
+                  className="w-full md:w-auto bg-green-600 text-white py-3 px-6 rounded-xl shadow-md hover:bg-green-700 transition-all"
+                >
+                  Create Chat
+                </button>
+              )}
+
+              {/* Delete Button */}
+              <button
+                onClick={openModal}
+                className="w-full md:w-auto bg-red-600 text-white py-3 px-6 rounded-xl shadow-md hover:bg-red-700 transition-all"
+              >
+                Delete Trip
+              </button>
+            </div>
+
+            {/* Modal for Confirmation */}
+            <Modal
+              isOpen={isModalOpen}
+              onRequestClose={closeModal}
+              className="flex items-center justify-center fixed inset-0 z-50 overflow-auto bg-black bg-opacity-50"
+              overlayClassName="fixed inset-0 bg-black bg-opacity-50"
+            >
+              <div className="bg-white rounded-lg shadow-lg max-w-md w-full p-6">
+                <h2 className="text-xl font-semibold mb-4 text-center">
+                  Are you sure you want to delete this trip?
+                </h2>
+                <div className="flex justify-around mt-6">
+                  <button
+                    onClick={handleDeleteTrip}
+                    className="bg-red-600 text-white py-2 px-4 rounded-lg shadow hover:bg-red-700 transition-all"
+                  >
+                    Yes, Delete
+                  </button>
+                  <button
+                    onClick={closeModal}
+                    className="bg-gray-300 text-gray-700 py-2 px-4 rounded-lg shadow hover:bg-gray-400 transition-all"
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </Modal>
+          </div>
+        )}
+      </div>
 
       <div className="flex items-center mb-6">
         <img
@@ -221,109 +275,90 @@ const TripDetail: React.FC<{ user: any }> = ({ user }) => {
       </div>
 
       {/* Chat Section */}
-      {trip.chatId && (
-        <div className="mt-10">
-          <h3 className="text-lg font-semibold text-gray-800">
-            Chat Messages:
-          </h3>
-          <ul className="space-y-3 mt-4">
-            {chatMessages.map((msg, idx) => (
-              <li key={idx} className="bg-gray-100 p-4 rounded-lg shadow-sm">
-                <strong className="text-gray-900">{msg.sender}:</strong>{" "}
-                {msg.text}
-              </li>
-            ))}
-          </ul>
+      {/* Chat Section */}
+      {trip.chatId &&
+        (trip.participants.some(
+          (participant) => participant.name === user?.displayName
+        ) ||
+          user?.displayName === trip.authorName) && (
+          <div className="mt-10">
+            <h3 className="text-lg font-semibold text-gray-800">
+              Chat Messages:
+            </h3>
+            <ul className="space-y-3 mt-4 max-h-64 overflow-y-auto">
+              {" "}
+              {/* Add max height and overflow */}
+              {chatMessages
+                .sort(
+                  (a, b) => a.createdAt?.toMillis() - b.createdAt?.toMillis()
+                )
+                .map((msg, idx) => {
+                  const messageTime = msg.createdAt
+                    ?.toDate()
+                    .toLocaleTimeString([], {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    });
 
-          <div className="flex mt-4">
-            <input
-              type="text"
-              value={newMessage}
-              onChange={(e) => setNewMessage(e.target.value)}
-              className="border border-gray-300 rounded-lg flex-grow px-4 py-2"
-              placeholder="Type a message..."
-            />
-            <button
-              onClick={handleSendMessage}
-              className="ml-2 bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition-all"
-            >
-              Send
-            </button>
-          </div>
-        </div>
-      )}
+                  const isUserMessage = msg.sender === user?.displayName;
+                  return (
+                    <li
+                      key={idx}
+                      className={`p-4 rounded-lg shadow-sm ${
+                        isUserMessage
+                          ? "bg-blue-600 text-white ml-auto"
+                          : "bg-gray-100 text-gray-900"
+                      }`}
+                    >
+                      <div className="flex justify-between">
+                        <strong className="text-sm">{msg.sender}:</strong>
+                        <span className="text-xs text-gray-500">
+                          {messageTime}
+                        </span>
+                      </div>
+                      <div>{msg.text}</div>
+                    </li>
+                  );
+                })}
+            </ul>
 
-      {/* Author-only Actions */}
-      {user?.displayName === trip.authorName && (
-        <div className="mt-2">
-          <div className="flex gap-2">
-            {!trip.chatId && (
+            <div className="flex mt-4">
+              <input
+                type="text"
+                value={newMessage}
+                onChange={(e) => setNewMessage(e.target.value)}
+                className="border border-gray-300 rounded-lg flex-grow px-4 py-2"
+                placeholder="Type a message..."
+              />
               <button
-                onClick={handleCreateChat}
-                className="w-full md:w-auto bg-green-600 text-white py-3 px-6 rounded-xl shadow-md hover:bg-green-700 transition-all"
+                onClick={handleSendMessage}
+                className="ml-2 bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition-all"
               >
-                Create Chat
+                Send
               </button>
-            )}
-
-            {/* Delete Button */}
-            <button
-              onClick={openModal}
-              className="w-full md:w-auto bg-red-600 text-white py-3 px-6 rounded-xl shadow-md hover:bg-red-700 transition-all"
-            >
-              Delete Trip
-            </button>
-
-            {user?.displayName && (
-              <div>
-                {trip.participants.some(
-                  (participant) => participant.name === user.displayName
-                ) ? (
-                  <button
-                    onClick={handleUnjoinTrip}
-                    className="w-full md:w-auto bg-red-800 text-white py-3 px-6 rounded-xl shadow-md hover:bg-red-700 transition-all"
-                  >
-                    Unjoin Trip
-                  </button>
-                ) : (
-                  <button
-                    onClick={handleJoinTrip}
-                    className="w-full md:w-auto bg-blue-600 text-white py-3 px-6 rounded-xl shadow-md hover:bg-blue-700 transition-all"
-                  >
-                    Join Trip
-                  </button>
-                )}
-              </div>
-            )}
-          </div>
-
-          {/* Modal for Confirmation */}
-          <Modal
-            isOpen={isModalOpen}
-            onRequestClose={closeModal}
-            className="flex items-center justify-center fixed inset-0 z-50 overflow-auto bg-black bg-opacity-50"
-            overlayClassName="fixed inset-0 bg-black bg-opacity-50"
-          >
-            <div className="bg-white rounded-lg shadow-lg max-w-md w-full p-6">
-              <h2 className="text-xl font-semibold mb-4 text-center">
-                Are you sure you want to delete this trip?
-              </h2>
-              <div className="flex justify-around mt-6">
-                <button
-                  onClick={handleDeleteTrip}
-                  className="bg-red-600 text-white py-2 px-4 rounded-lg shadow hover:bg-red-700 transition-all"
-                >
-                  Yes, Delete
-                </button>
-                <button
-                  onClick={closeModal}
-                  className="bg-gray-300 text-gray-700 py-2 px-4 rounded-lg shadow hover:bg-gray-400 transition-all"
-                >
-                  Cancel
-                </button>
-              </div>
             </div>
-          </Modal>
+          </div>
+        )}
+
+      {user?.displayName && (
+        <div>
+          {trip.participants.some(
+            (participant) => participant.name === user.displayName
+          ) ? (
+            <button
+              onClick={handleUnjoinTrip}
+              className="w-full md:w-auto mt-2 bg-red-800 text-white py-3 px-6 rounded-xl shadow-md hover:bg-red-700 transition-all"
+            >
+              Leave Trip
+            </button>
+          ) : (
+            <button
+              onClick={handleJoinTrip}
+              className="w-full md:w-auto mt-2 bg-blue-600 text-white py-3 px-6 rounded-xl shadow-md hover:bg-blue-700 transition-all"
+            >
+              Join Trip
+            </button>
+          )}
         </div>
       )}
 
